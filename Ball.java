1111c2cf6e83a3e098bf84acec3469c3f89cd160
chrisseto <chriskseto@gmail.com>
2013-10-20 11:34:03 -0400

    Updated Ball

--- Ball.java
+++ Ball.java
@@ -2,7 +2,7 @@ package com.ChrisSeto.BallDotJava
 
 public abstract Ball
 {
-	private Point location;
+	private BVector location;
 	private int size;
 	Ball(int x, int y, int size)
 	{
@@ -17,5 +17,28 @@ public abstract Ball
 	public int getY()
 	{
 		return location.y;
+	}
+
+	public boolean checkCollision(Ball other)
+  	{
+    		return(PVector.sub(other.position,position).magSq() <= ((size+other.size)/2)*((size+other.size)/2));  
+  	}
+
+	void update()
+	{
+		position.add(velocity);
+		checkBoundaryCollition();
+	}
+	
+	void checkBoundaryCollition() //I'm broken still :[
+	{
+		if (position.x > width-size/2)
+			position.x = width-size/2;
+		if (position.x <size/2)
+			position.x = size/2;
+		if (position.y > height-size/2)
+			position.y = height-size/2;
+		if (position.y< size/2)
+			position.y =size/2;
 	}
 }
