d23dcc6adb7cb608e9ead514b318aa9980c8568a
chrisseto <chriskseto@gmail.com>
2013-10-21 23:20:04 -0400

    More files

---
+++ Assests.java
@@ -0,0 +1,11 @@
+package com.ChrisSeto.JavaDotBall
+
+//This class is purely for resources and what not
+
+public class Assets
+{
+	public static final int WIDTH = 800;
+	public static final int HEIGHT = 800;
+	public static final int PLAYERSIZE = 20;
+	public static final int ENEMYSIZE = 12;
+}
---
+++ PowerUp.java
@@ -0,0 +1,20 @@
+class PowerUp extends Ball
+{
+  Timer life;
+  boolean done;
+  int type;
+  PowerUp()
+  {
+
+    super(new PVector(random(11, 800), random(11,500)), POWERUPSIZE, 1, color(0, 250, 0));
+    done = false;
+    life = new Timer(12);
+    type = (int)random(0,PowerUpTypes.length);
+  }
+  void draw()
+  {
+    done = life.checkDone();
+    super.draw();
+  }
+  //abstract Effect getEffect();
+}
